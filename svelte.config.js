a4a6d2b23b61796c9a82f49ad5488d46d83c0393
marktrs <traisak.tra@gmail.com>
2023-04-24 18:13:38 +0700

    setup project config

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
-import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-auto'
+import { vitePreprocess } from '@sveltejs/kit/vite'
+import path from 'path'
+import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -17,8 +18,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			$store: path.resolve('src/store.ts')
+		}
 	}
-};
+}
 
-export default config;
+export default config
